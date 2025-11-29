afcedbec15835efd1181db5fc2e2f4633a6e1cec
nekename <63245705+nekename@users.noreply.github.com>
2025-11-29 13:38:18 +0000

    cleanup

--- src/components/InstanceEditor.svelte
+++ src/components/InstanceEditor.svelte
@@ -15,25 +15,23 @@
 	let fileInput: HTMLInputElement;
 	let colourInput: HTMLInputElement;
 
-	let isDragging = false;
-
 	function handleDrop(event: DragEvent) {
 		event.preventDefault();
-		isDragging = false;
 
 		const file = event.dataTransfer?.files?.[0];
-		if (file && file.type.startsWith("image/")) {
-			const reader = new FileReader();
-			reader.onload = async () => {
-				let result = reader.result?.toString();
-				if (result) {
-					let resized = await resizeImage(result);
-					if (resized) instance.states[state].image = resized;
-					else instance.states[state].image = result;
-				}
-			};
-			reader.readAsDataURL(file);
-		}
+		if (!file || !file.type.startsWith("image/")) return;
+		const reader = new FileReader();
+
+		reader.onload = async () => {
+			let result = reader.result?.toString();
+			if (result) {
+				let resized = await resizeImage(result);
+				if (resized) instance.states[state].image = resized;
+				else instance.states[state].image = result;
+			}
+		};
+
+		reader.readAsDataURL(file);
 	}
 
 	function update(instance: ActionInstance) {
@@ -65,15 +63,7 @@
 		<div class="flex flex-col justify-center items-center">
 			<button
 				on:drop={handleDrop}
-				on:dragstart={() => {
-					isDragging = true;
-				}}
-				on:dragover={(event) => {
-					event.preventDefault();
-				}}
-				on:dragleave={() => {
-					isDragging = false;
-				}}
+				on:dragover={(event) => event.preventDefault()}
 				on:click={() => fileInput.click()}
 				on:contextmenu={(event) => {
 					event.preventDefault();
